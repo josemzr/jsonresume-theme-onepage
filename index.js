b52cfa2f53e44a6c986f5873e0de51259a208a20
Jose Manzaneque Ramos <jmm0693@gmail.com>
2018-03-15 14:14:06 -0700

    Updated CV and style

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ COURSES_COLUMNS = 3;
 PREPEND_SUMMARY_CATEGORIES = [
   "work",
   "volunteer",
-  "awards",
+  "certifications",
   "publications"
 ];
+
+Handlebars.registerHelper("smallDate", function (datetime) {
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  var date = new Date(datetime);
+  return months[date.getMonth()] + " " + date.getFullYear();
+});
 
 function validateArray(arr) {
   return arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0;
@@ -17,14 +23,14 @@ function validateArray(arr) {
 function render(resume) {
   // Split courses into 3 columns
   if (validateArray(resume.education)) {
-    resume.education.forEach(function(block) {
+    resume.education.forEach(function (block) {
       if (validateArray(block.courses)) {
         splitCourses = [];
         columnIndex = 0;
         for (var i = 0; i < COURSES_COLUMNS; i++) {
           splitCourses.push([]);
         }
-        block.courses.forEach(function(course) {
+        block.courses.forEach(function (course) {
           splitCourses[columnIndex].push(course);
           columnIndex++;
           if (columnIndex >= COURSES_COLUMNS) {
@@ -36,9 +42,9 @@ function render(resume) {
     });
   }
 
-  PREPEND_SUMMARY_CATEGORIES.forEach(function(category) {
+  PREPEND_SUMMARY_CATEGORIES.forEach(function (category) {
     if (resume[category] !== undefined) {
-      resume[category].forEach(function(block) {
+      resume[category].forEach(function (block) {
         if (block.highlights === undefined) {
           block.highlights = [];
         }
@@ -50,14 +56,43 @@ function render(resume) {
     }
   });
 
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
+  //categorize work by company
+  if (resume.work.length > 0) {
+    var categorizedWork = {};
+
+    resume.work.forEach(function(job){
+      if (!categorizedWork.hasOwnProperty(job.company)) {
+        var companyDescription = "";
+        if (job.hasOwnProperty("description")) {
+          companyDescription = job.description;
+        }
+        categorizedWork[job.company] = {
+          "name": job.company,
+          "description": companyDescription,
+          "positions": []
+        };
+      }
+
+      categorizedWork[job.company].positions.push(job);
+    });
+
+    var workArray = [];
+    for (var idx in categorizedWork) {
+      var work = categorizedWork[idx];
+      workArray.push(work);
+    }
+
+    resume.work = workArray;
+  } 
+
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume
+  });
 }
 
 module.exports = {
-	render: render
+  render: render
 };
